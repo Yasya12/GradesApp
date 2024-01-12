063af9841a027ad4e83e79a922f0fc24a388e935
Yaryna Laiter <jasja200411@gmail.com>
2024-01-12 03:34:54 +0200

    A-1.0 - added some entities/repositories

---
+++ Grades.Persistence/Repositories/Semester.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Grades.Persistence.Repositories
+{
+	public class Semester
+	{
+		private int number;
+		private int startYear;
+
+		public int Number
+		{
+			get => number;
+			set
+			{
+				if (value == 1 || value == 2)
+				{
+					number = value;
+				}
+				else
+				{
+					throw new ArgumentException("Number must be 1 or 2.");
+				}
+			}
+		}
+
+		public int StartYear
+		{
+			get => startYear;
+			set => startYear = value;
+		}
+
+		public int EndYear
+		{
+			get => StartYear + 1;
+		}
+
+		public Semester(int number, int startYear)
+		{
+			Number = number;
+			StartYear = startYear;
+		}
+
+		public string GetSemesterInfo()
+		{
+			return $"{Number} семестр {StartYear}/{EndYear}";
+		}
+	}
+}
