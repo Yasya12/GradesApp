c6405ad3fe31172e43dcf0acdb1fc153731ae831
jasja <jasja200411@gmail.com>
2024-07-25 18:36:01 +0300

    W3 - create interfaces for new models

---
+++ GradesApp.Domain/Interfaces/Repositories/IGroupRepository.cs
@@ -0,0 +1,8 @@
+using System.Text.RegularExpressions;
+
+namespace GradesApp.Domain.Interfaces.Repositories;
+
+public interface IGroupRepository : IGenericRepository<Group>
+{
+    
+}
---
+++ GradesApp.Domain/Interfaces/Repositories/ISemesterRepository.cs
@@ -0,0 +1,8 @@
+using GradesApp.Domain.Entities;
+
+namespace GradesApp.Domain.Interfaces.Repositories;
+
+public interface ISemesterRepository : IGenericRepository<Semester>
+{
+    
+}
