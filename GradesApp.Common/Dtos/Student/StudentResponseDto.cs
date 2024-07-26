b552336be3a2f60a33c5c17025049a7c9256010c
jasja <jasja200411@gmail.com>
2024-07-26 20:07:40 +0300

    W4 - feat: Create StudentResponseDto

---
+++ GradesApp.Common/Dtos/Student/StudentResponseDto.cs
@@ -0,0 +1,10 @@
+namespace GradesApp.Application.Dtos;
+
+public class StudentResponseDto
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string UserEmail { get; set; } 
+    public string GroupName { get; set; }
+    public string SpecialityName { get; set; }
+}
