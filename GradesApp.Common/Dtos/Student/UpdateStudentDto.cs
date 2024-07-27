b4984159488efa20f05e5cd444d92713e387b3c6
jasja <jasja200411@gmail.com>
2024-07-27 15:57:28 +0300

    W4 - refactor: Remove unnecessary line from StudentUpdateDto

--- GradesApp.Common/Dtos/Student/UpdateStudentDto.cs
+++ GradesApp.Common/Dtos/Student/UpdateStudentDto.cs
@@ -2,7 +2,6 @@ namespace GradesApp.Application.Dtos;
 
 public class UpdateStudentDto
 {
-    public Guid Id { get; set; }  
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public DateTime DateOfBirth { get; set; }
