ebfae44710f3205d1a723c247f5f25ff78b49f21
jasja <jasja200411@gmail.com>
2024-07-28 15:17:17 +0300

    W4-b - fix: Update Faculty and Speciality Entities
    - Made a field in the Faculty entity optional.
    - Used [JsonIgnore] in the Speciality entity to avoid cyclic dependencies.

--- GradesApp.Domain/Entities/Faculty.cs
+++ GradesApp.Domain/Entities/Faculty.cs
@@ -5,5 +5,5 @@ public class Faculty
     public Guid Id { get; set; }
     public string Name { get; set; }
     public string? Description { get; set; }
-    public List<Speciality> Specialities { get; set; }
+    public List<Speciality>? Specialities { get; set; }
 }
--- GradesApp.Domain/Entities/Speciality.cs
+++ GradesApp.Domain/Entities/Speciality.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace GradesApp.Domain.Entities;
 
 public class Speciality
@@ -6,6 +8,7 @@ public class Speciality
     public string Name { get; set; }
     public string Code { get; set; }
     public Guid FacultyId { get; set; }
+    [JsonIgnore]
     public Faculty Faculty { get; set; }
     public List<Group> Groups { get; set; }
 }
