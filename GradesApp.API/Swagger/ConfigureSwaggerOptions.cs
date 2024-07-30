8f7e28b66535e150bbab2de17d96cdeb225c7cba
jasja <jasja200411@gmail.com>
2024-07-30 15:24:51 +0300

    W4-c - config: Add ConfigureSwaggerOptions for Swagger configuration

---
+++ GradesApp.API/Swagger/ConfigureSwaggerOptions.cs
@@ -0,0 +1,42 @@
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace GradesApp.API.Swagger;
+
+public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+{
+    public void Configure(SwaggerGenOptions options)
+    {
+        options.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API", Version = "v1" });
+
+        var securityScheme = new OpenApiSecurityScheme
+        {
+            Name = "JWT Authentication",
+            Description = "Enter your JWT token in this field",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.Http,
+            Scheme = "bearer",
+            BearerFormat = "JWT"
+        };
+
+        options.AddSecurityDefinition("Bearer", securityScheme);
+
+        var securityRequirement = new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                new string[] { }
+            }
+        };
+
+        options.AddSecurityRequirement(securityRequirement);
+    }
+}
