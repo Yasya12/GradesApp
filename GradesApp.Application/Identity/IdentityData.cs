dcc06dff7e1929df84ebdbec1480225e936c9660
jasja <jasja200411@gmail.com>
2024-07-30 15:21:15 +0300

    W4-c - feat: Create CustomClaimsDto for token claims and  IdentityData for storing claims and policies

---
+++ GradesApp.Application/Identity/IdentityData.cs
@@ -0,0 +1,7 @@
+namespace GradesApp.Application.Identity;
+
+public class IdentityData
+{
+    public const string AdminUserClaimName = "admin";
+    public const string AdminUserPolicyName = "Admin";
+}
