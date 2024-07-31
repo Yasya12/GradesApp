b3f16f3c7b996998523adc865fb61567d28b67d7
jasja <jasja200411@gmail.com>
2024-07-31 18:33:49 +0300

    W4-c - fix: Update LoginDto to use user roles instead of custom claims

--- GradesApp.Common/Dtos/Authentication/CustomClaimsDto.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel;
-
-namespace GradesApp.Common.Dtos.Authentication;
-
-public class CustomClaimsDto
-{
-    [DefaultValue(false)]
-    public bool admin { get; set; }
-}
--- GradesApp.Common/Dtos/Authentication/LoginDto.cs
+++ GradesApp.Common/Dtos/Authentication/LoginDto.cs
@@ -2,8 +2,6 @@ namespace GradesApp.Common.Dtos.Authentication;
 
 public class LoginDto
 {
-    public string UserId { get; set; }
     public string UserName { get; set; }
     public string Password { get; set; }
-    public CustomClaimsDto CustomClaims { get; set; }
 }
